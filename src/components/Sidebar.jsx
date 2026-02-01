ef8adb21e5a2993137a1ad73392dba6d69bf3882
VICTOR <omeifevictor001@gmail.com>
2026-02-01 23:10:30 +0100

    Removed unnecessary links in the Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -62,26 +62,6 @@ export default function Sidebar() {
             <span>Community</span>
           </NavLink>
           <NavLink
-            to="/courses"
-            className={({ isActive }) =>
-              `flex items-center space-x-3 rounded-md p-2 ${isActive ? 'bg-card-bg text-primary-purple font-medium' : 'hover:bg-card-bg'
-              }`
-            }
-          >
-            <AcademicCapIcon className="size-5" />
-            <span>Courses</span>
-          </NavLink>
-          <NavLink
-            to="/tools"
-            className={({ isActive }) =>
-              `flex items-center space-x-3 rounded-md p-2 ${isActive ? 'bg-card-bg text-primary-purple font-medium' : 'hover:bg-card-bg'
-              }`
-            }
-          >
-            <WrenchScrewdriverIcon className="size-5" />
-            <span>Tools</span>
-          </NavLink>
-          <NavLink
             to="/reports"
             className={({ isActive }) =>
               `flex items-center space-x-3 rounded-md p-2 ${isActive ? 'bg-card-bg text-primary-purple font-medium' : 'hover:bg-card-bg'
