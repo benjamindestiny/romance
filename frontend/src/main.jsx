e80041057d2b0f3629935debe0943bc7a1550824
unknown <benjamindestiny>
2026-02-04 15:39:27 -0900

    Save local PWA and frontend changes before merging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
+
 
 
 createRoot(document.getElementById("root")).render(
@@ -12,3 +14,12 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </StrictMode>
 );
+
+registerSW({
+  onNeedRefresh() {
+    console.log("New update available");
+  },
+  onOfflineReady() {
+    console.log("App ready to work offline");
+  }
+});
