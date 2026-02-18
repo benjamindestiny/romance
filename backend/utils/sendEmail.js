513bde3224db433adb09e297040475cb5bebc00b
unknown <benjamindestiny>
2026-02-17 18:18:18 -0900

    changed ome backend files

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,11 +1,18 @@
 import { Resend } from "resend";
 
+console.log("API KEY:", process.env.RESEND_API_KEY);
+
+
+if (!process.env.RESEND_API_KEY) {
+  throw new Error("RESEND_API_KEY is missing in environment variables");
+}
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      from: "onboarding@resend.dev", // temporary default sender
+      from: "onboarding@resend.dev",
       to,
       subject,
       html,
